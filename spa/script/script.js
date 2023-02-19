23cb3c48888ce812ad72ee3eb658c7c8ea6dcaae
K3A101 <keisha.alexander@hva.nl>
2023-02-19 10:25:36 +0100

    loading state op header, avatar en title

--- spa/script/script.js
+++ spa/script/script.js
@@ -2,13 +2,15 @@ console.log("Hello");
 //Variabele
  const main = document.querySelector("main ");
 const section = document.getElementById('content');
-const loader = document.querySelector('.loading');
+const loader = document.querySelectorAll('.loading');
 console.log(section)
  
 //Logica
 fetchData()
 
+replaceUserImg()
 
+replaceAppName()
 
 // Hier ga ik data ophalen uit met de fetch API
 function fetchData() {
@@ -27,6 +29,7 @@ function fetchData() {
 }
 // Met deze functie plaats ik de data in de HTML
 function addData(data) {
+      
        
         let html = '' 
         data.forEach(item => {
@@ -65,14 +68,45 @@ function checkError(response) {
 
 function showLoader() {
 
-  loader.classList.add("display");
-  setTimeout(() => {
-    loader.classList.remove("display");
-  }, 1000)
+  loader.forEach(item =>{
+    item.classList.add("display");
+    setTimeout(() => {
+      item.classList.remove("display");
+    }, 1000)
+  })
+
+
 }
 
 function hideLoader() {
-  loader.classList.remove("display");
+  loader.forEach(item => {
+  item.classList.remove("display");
+  })
+ 
+}
+
+// Nadat de pagina wordt geladen worden de container vervangen door een afbeelding
+function replaceUserImg() {
+  const imageContainer = document.getElementById('image-container');
+  // Hier maak ik een nieuwe img tag
+  const image = new Image()
+  image.src = "../spa/images/mijn-foto.jpg";
+  image.addEventListener('load', () => {
+    imageContainer.style.backgroundImage = `url(${image.src})`; /* Replace the background color with the loaded image */
+    imageContainer.classList.remove('placeholder');
+  })
+  
+}
+
+// Wanneer de pagina geladen is wordt de titel design quotes zichtbaar
+function replaceAppName() {
+  const textTitle = document.getElementById("text-title");
+  // const appTitle = document.querySelector('loading-app-name');
+  
+  window.addEventListener('load', () => {
+    textTitle.innerText = "Design quotes";
+    textTitle.classList.remove('loading-app-name');
+  })
 }
 
 // Bron: https://dev.to/vaishnavs/displaying-loading-animation-on-fetch-api-calls-1e5m
